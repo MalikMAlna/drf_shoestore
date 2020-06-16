ada279098ecc1ef96175346ec79a6497990b9965
Mike A <jimalik2525@gmail.com>
2020-06-16 16:40:04 -0400

    Finished with viewsets now adding routing for api endpoints

--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# TODO Add viewsets
+from api.serializers import ManufacturerSerializer, ShoeColorSerializer, ShoeTypeSerializer, ShoeSerializer
+from shoestore.models import Manufacturer, ShoeColor, ShoeType, Shoe
+
+
+class ManufacturerViewSet(ModelViewSet):
+    serializer_class = ManufacturerSerializer
+    basename = 'manufacturer'
+    queryset = Manufacturer.objects.all()
+
+
+class ShoeColorViewSet(ModelViewSet):
+    serializer_class = ShoeColorSerializer
+    basename = 'color'
+    queryset = ShoeColor.objects.all()
+
+
+class ShoeTypeViewSet(ModelViewSet):
+    serializer_class = ShoeTypeSerializer
+    basename = 'type'
+    queryset = ShoeType.objects.all()
+
+
+class ShoeViewSet(ModelViewSet):
+    serializer_class = ShoeSerializer
+    basename = 'shoe'
+    queryset = Shoe.objects.all()
